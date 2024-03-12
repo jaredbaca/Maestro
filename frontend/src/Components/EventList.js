34f871dabee16bd8fe69bc5c83819c8e880a618f
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2024-03-12 10:10:16 -0400

    added date arrows

--- frontend/src/Components/EventList.js
+++ frontend/src/Components/EventList.js
@@ -5,7 +5,8 @@ import moment from 'moment';
 import {useNavigate} from 'react-router-dom';
 
 import "react-datepicker/dist/react-datepicker.css";
-import { FloatingLabel } from 'react-bootstrap';
+import { FloatingLabel, Button } from 'react-bootstrap';
+import {ChevronBarLeft, ChevronLeft, ChevronRight } from 'react-bootstrap-icons';
 
 function EventList(props) {
 
@@ -13,9 +14,27 @@ function EventList(props) {
 
     const [events, setEvents] = useState([]);
     const [date, setDate] = useState(new Date());
-
     const defaultDate = new Date().toISOString().slice(0,10);
-    console.log(defaultDate);
+    // const [testDate] = useState(new Date());
+    // const [testDateString, setTestDateString] = useState(testDate.toDateString());
+    // console.log(defaultDate);
+
+    // Decrement date with arrow button
+    const handleDecrementDate = () => {
+        let tempDate = new Date(date);
+        tempDate.setDate(tempDate.getDate() - 1);
+        console.log(`temp date: ${tempDate}`)
+        setDate(tempDate);
+        console.log(`date: ${date}`)
+    }
+
+    const handleIncrementDate = () => {
+        let tempDate = new Date(date);
+        tempDate.setDate(tempDate.getDate() + 1);
+        console.log(`temp date: ${tempDate}`)
+        setDate(tempDate);
+        console.log(`date: ${date}`)
+    }
     
 
     useEffect(() => {
@@ -47,7 +66,9 @@ function EventList(props) {
         <div className="events-container">
 
             <div className="text-center mt-3 mb-3">
-                <input type="date" id="dateSelect" name="date" defaultValue={defaultDate} onChange={(event)=>setDate(event.target.value)}/>
+                <ChevronLeft className='pr-4' onClick={handleDecrementDate}/>
+                <input type="date" id="dateSelect" name="date"  defaultValue={defaultDate} value={date.toISOString().slice(0,10)} onChange={(event)=>setDate(new Date(event.target.value))}/>
+                <ChevronRight className='ml-5' onClick={handleIncrementDate}/>
             </div>
             
             {/* <h5>{date.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })}</h5> */}
